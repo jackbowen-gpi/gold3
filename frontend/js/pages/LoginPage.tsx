d8b03ff9f23a38f1879270ffd0925377b0ef74f9
jackbowen-gpi <jack.bowen@graphicpkg.com>
2025-08-24 13:41:56 -0400

    fix(ui): remove duplicate AuthHeader from login pages

--- frontend/js/pages/LoginPage.tsx
+++ frontend/js/pages/LoginPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { LoginForm } from '../../src/features/authentication/components/LoginForm';
-import { AuthHeader } from '../../src/features/authentication/components/AuthHeader';
 
 const LoginPage: React.FC = () => (
   <div>
-    <AuthHeader />
     <main style={{ padding: 24 }}>
       <LoginForm />
     </main>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { LoginForm } from '../features/authentication/components/LoginForm';
-import { AuthHeader } from '../features/authentication/components/AuthHeader';
 
 export const LoginPage: React.FC = () => (
   <div>
-    <AuthHeader />
     <main style={{ padding: 24 }}>
       <LoginForm />
     </main>
